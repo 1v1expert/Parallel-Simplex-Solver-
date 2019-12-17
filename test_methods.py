4cbb627ab04fd694f687945505502d411bdc9f71
1v1expert <1v1expert@gmail.com>
2019-12-17 17:00:01 +0300

    upd#3

--- test_methods.py
+++ test_methods.py
@@ -18,7 +18,7 @@ class MainTestCase(object):
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.pprint:
-            print('Time: {}'.format(time.time() - self.start_test))
+            print('Full time: {}'.format(time.time() - self.start_test))
         return False
     
     def start(self, A=[[]], b=[], c=[], method='revised simplex'):
@@ -29,7 +29,6 @@ class MainTestCase(object):
             print(result)
             
         return result
-            # print('Time: {}'.format(time.time() - self.start()))
 
 # def test2():
 # revised_simplex = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds), method='')
@@ -38,9 +37,17 @@ class MainTestCase(object):
 
 
 with MainTestCase(pprint=True) as test:
-    test.start(A=[[3, 7], [0, 5], [-1, 0]], b=[79, 42, -3], c=[-2, -45])
+    """ Maximize(2*X1 + 45*X2)
+    subject to
+    3*x1	+	7*x2	≤	79
+    0*x1    +   5*x2	≤	42
+    -x1     +   0*x2 	≤	-3
+    """
+    result = test.start(A=[[3, 7], [0, 5], [-1, 0]], b=[79, 42, -3], c=[-2, -45])
+    if result.success:
+        print(result.x)
     
 
 # SimplexSolver().run_simplex(A=[[2, 1, 0], [1, 2, -2], [0, 1, 2]], b=[10, 20, 5], c=[2, -1, 2])
-SimplexSolver().run_simplex(A=[[3, 7], [0, 5], [-1, 0]], b=[79, 42, -3], c=[-2, -45])
+# SimplexSolver().run_simplex(A=[[3, 7], [0, 5], [-1, 0]], b=[79, 42, -3], c=[-2, -45])
 
