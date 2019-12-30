c3ca082ba651cde78ce4fe00fd02a63a6428fbd2
1v1expert <1v1expert@gmail.com>
2019-12-30 20:27:37 +0300

    quotes changed

--- simplex_test.py
+++ simplex_test.py
@@ -9,14 +9,14 @@ class SimplexSolverTest(unittest.TestCase):
 
     ''' STANDARD FORM MAXIMIZATION EXAMPLES '''
     def test_max_feasible1(self):
-        ''' Find optimal solution to max(x1 + x2) such that:
+        """ Find optimal solution to max(x1 + x2) such that:
             Ax <= b,
             x1, x2 >= 0,
             A =  |2 1|, b = |4|, c = |1|
                  |1 2|      |3|      |1|
 
             Source: http://mat.gsia.cmu.edu/classes/QUANT/NOTES/chap7.pdf, Example 7.2.1
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(5,3),
                               'x_2': Fraction(2,3),
                               's_1': Fraction(0),
@@ -28,7 +28,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           [1,1]))
 
     def test_max_feasible2(self):
-        ''' Find optimal solution to max(4x1 + 6x2) such that:
+        """ Find optimal solution to max(4x1 + 6x2) such that:
             Ax <= b,
             x1, x2 >= 0,
             A =  |-1 1|, b = |11|, c = |4|
@@ -36,7 +36,7 @@ class SimplexSolverTest(unittest.TestCase):
                  | 2 5|      |90|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s3.pdf, Example 1
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(15),
                               'x_2': Fraction(12),
                               's_1': Fraction(14),
@@ -50,7 +50,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           [4,6]))
 
     def test_max_feasible3(self):
-        ''' Find optimal solution to max(2x1 - x2 + 2x3) such that:
+        """ Find optimal solution to max(2x1 - x2 + 2x3) such that:
             Ax <= b,
             x1, x2 >= 0,
             A =  |2 1  0|, b = |10|, c = | 2|
@@ -58,7 +58,7 @@ class SimplexSolverTest(unittest.TestCase):
                  |0 1  2|      | 5|      | 2|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s3.pdf, Example 2
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(5),
                               'x_2': Fraction(0),
                               'x_3': Fraction(5,2),
@@ -73,7 +73,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           [2,-1,2]))
 
     def test_max_feasible4(self):
-        ''' Find optimal solution to max(2x1 - x2 + 2x3) such that:
+        """ Find optimal solution to max(2x1 - x2 + 2x3) such that:
             Ax <= b,
             x1, x2, x3 >= 0,
             A =  |4 1 1|, b = |30|, c = |3|
@@ -81,7 +81,7 @@ class SimplexSolverTest(unittest.TestCase):
                  |1 2 3|      |40|      |1|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s3.pdf, Example 3
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(3),
                               'x_2': Fraction(18),
                               'x_3': Fraction(0),
@@ -96,7 +96,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           [3,2,1]))
 
     def test_max_feasible5(self):
-        ''' Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
+        """ Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
             Ax <= b,
             x1, x2, x3 >= 0,
             A =  | 1   2  3/2|, b = |12000|, c = |11|
@@ -104,7 +104,7 @@ class SimplexSolverTest(unittest.TestCase):
                  |1/2 1/3 1/2|      | 2400|      |15|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s3.pdf, Example 4
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(600),
                               'x_2': Fraction(5100),
                               'x_3': Fraction(800),
@@ -122,7 +122,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           [11,16,15]))
 
     def test_max_feasible6(self):
-        ''' Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
+        """ Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
             Ax <= b,
             x1, x2, x3 >= 0,
             A =  |20  6  3|, b = |182|, c = |100000|
@@ -131,7 +131,7 @@ class SimplexSolverTest(unittest.TestCase):
                  |-9  1  1|      |  0|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s3.pdf, Example 5
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(4),
                               'x_2': Fraction(10),
                               'x_3': Fraction(14),
@@ -150,14 +150,14 @@ class SimplexSolverTest(unittest.TestCase):
                                                            18000]))
     ''' STANDARD FORM MINIMIZATION PROBLEMS '''
     def test_min_feasible1(self):
-        ''' Find optimal solution to min(3x1 + 2x2) such that:
+        """ Find optimal solution to min(3x1 + 2x2) such that:
             Ax >= b,
             x1, x2 >= 0,
             A = |2, 1|, b = |6|, c = |3|
                 |1, 1|      |4|      |2|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s4.pdf, Example 1
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(2),
                               'x_2': Fraction(2),
                               's_1': Fraction(0),
@@ -172,7 +172,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           prob='min'))
 
     def test_min_feasible2(self):
-        ''' Find optimal solution to min(2x1 + 10x2 + 8x3) such that:
+        """ Find optimal solution to min(2x1 + 10x2 + 8x3) such that:
             Ax >= b,
             x1, x2, x3 >= 0,
             A = | 1 1 1|, b = |6|, c = | 2|
@@ -180,7 +180,7 @@ class SimplexSolverTest(unittest.TestCase):
                 |-1 2 2|      |4|      | 8|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s4.pdf, Example 2
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(2),
                               'x_2': Fraction(0),
                               'x_3': Fraction(4),
@@ -199,7 +199,7 @@ class SimplexSolverTest(unittest.TestCase):
                                                           prob='min'))
 
     def test_min_feasible3(self):
-        ''' Find optimal solution to min(20000x1 + 25000x2) such that:
+        """ Find optimal solution to min(20000x1 + 25000x2) such that:
             Ax >= b,
             x1, x2 >= 0,
             A = |400 300|, b = |25000|, c = |20000|
@@ -207,7 +207,7 @@ class SimplexSolverTest(unittest.TestCase):
                 |200 500|      |30000|
 
             Source: http://college.cengage.com/mathematics/larson/elementary_linear/4e/shared/downloads/c09s4.pdf, Example 3
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(25),
                               'x_2': Fraction(50),
                               's_1': Fraction(0),
@@ -228,16 +228,16 @@ class SimplexSolverTest(unittest.TestCase):
 
 class SimplexLinprogSolverTest(unittest.TestCase):
 
-    ''' STANDARD FORM MAXIMIZATION EXAMPLES '''
+    """ STANDARD FORM MAXIMIZATION EXAMPLES """
     def test_max_feasible1(self):
-        ''' Find optimal solution to max(x1 + x2) such that:
+        """ Find optimal solution to max(x1 + x2) such that:
             Ax <= b,
             x1, x2 >= 0,
             A =  |2 1|, b = |4|, c = |1|
                  |1 2|      |3|      |1|
 
             Source: http://mat.gsia.cmu.edu/classes/QUANT/NOTES/chap7.pdf, Example 7.2.1
-        '''
+        """
         self.assertDictEqual({'x_1': Fraction(5, 3),
                               'x_2': Fraction(2, 3),
                               's_1': Fraction(0),
